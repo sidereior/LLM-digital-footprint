bf5f1959385e3124b0bbf9a20c84ad0c92d05735
sidereior <alexanderlnanda@gmail.com>
2023-11-02 18:24:24 -0400

    jurrasic 2 working somewhat

--- j2processor.py
+++ j2processor.py
@@ -1,9 +1,8 @@
-from langchain.llms import OpenAI
+from langchain.llms import AI21
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 
-llm = OpenAI(temperature=0.9)
+llm = AI21(temperature=0.9) 
 tools = load_tools(["serpapi"], llm=llm)
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-agent.run("tell me about Alexander Nanda, a student at Dartmouth college. Be sure to be specific and talk about all aspects of Alexander Nanda.")
-#
+agent.run("Who does Messi play for")
